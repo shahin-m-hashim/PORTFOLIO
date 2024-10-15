1ff1383395e53ddde300a2bb04a32ed118272219
Shahin M Hashim <shahinmhashim@gmail.com>
2024-10-15 22:36:11 +0530

    created custom mouse cursor

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 window.onload = function () {
   console.log("Welcome To My Portfolio Website");
 
+  let cursorX = 0;
+  let cursorY = 0;
+  const smoothness = 0.15;
+
+  window.addEventListener("mousemove", updateCursor);
+  const bubbleCursor = document.getElementById("bubbleCursor");
+
+  function updateCursor(e) {
+    const dx = e.clientX - cursorX;
+    const dy = e.clientY - cursorY;
+
+    cursorX += dx * smoothness;
+    cursorY += dy * smoothness;
+
+    const centerX = cursorX - bubbleCursor.clientWidth / 2;
+    const centerY = cursorY - bubbleCursor.clientHeight / 2;
+
+    bubbleCursor.style.top = `${centerY}px`;
+    bubbleCursor.style.left = `${centerX}px`;
+  }
+
+  const navLinks = document.querySelectorAll("button, a");
+
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener("mouseenter", () => {
+      bubbleCursor.style.transform = "scale(1.3)";
+      bubbleCursor.style.backdropFilter = "blur(0.5px)";
+    });
+
+    navLink.addEventListener("mouseleave", () => {
+      bubbleCursor.style.transform = "scale(1)";
+      bubbleCursor.style.backdropFilter = "blur(10px)";
+    });
+  });
+
+  const projectLinks = document.querySelectorAll("#prLink");
+  const cursorContent = document.getElementById("cursorContent");
+
+  projectLinks.forEach((prLink) => {
+    prLink.addEventListener("mouseenter", () => {
+      cursorContent.style.opacity = "1";
+    });
+
+    prLink.addEventListener("mouseleave", () => {
+      cursorContent.style.opacity = "0";
+    });
+  });
+
   // slogan
   new Typed("#slogTxt1", {
     strings: ["Websites And Web Applications"],
